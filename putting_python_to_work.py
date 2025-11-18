64c20c7cc16179256fca09ed26c91bf0f49eb06e
AYANSHINA AMOS <skyon2016@gmail.com>
2025-11-18 13:22:52 +0100

    i add putting_python_to_test

--- putting_python_to_work.py
+++ putting_python_to_work.py
@@ -7,3 +7,13 @@
 # TO-DO: calculate the area of the circle
 
 # TO-DO: print the values for the user
+
+
+radius = float(input("Enter the radius of the circle: "))
+diameter = 2 * radius
+circumference = 2 * 3.14 * radius
+area = 3.14 * (radius ** 2)
+print("\nCircle Calculations:")
+print(f"Diameter: {diameter}")
+print(f"Circumference: {circumference}")
+print(f"Area: {area}")
